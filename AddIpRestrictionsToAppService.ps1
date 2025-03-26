4d0c4f310f5ec25c70954c64fcc178d18f4ddab6
Callum Errington <callum.errington@coxautoinc.com>
2025-03-26 16:08:37 +0000

    Updated ip restriction

--- AddIpRestrictionsToAppService.ps1
+++ AddIpRestrictionsToAppService.ps1
@@ -16,21 +16,16 @@ Connect-AzAccount
 
 # Iterate through each new IP restriction
 foreach ($rule in $newIpRestrictions) {
-    # Check if the IP address already exists
-    if (-not $existingIpAddresses.ContainsKey($rule.IpAddress)) {
-        # Add the new access restriction rule
-        Add-AzWebAppAccessRestrictionRule -ResourceGroupName $resourceGroupName `
-            -WebAppName $appServiceName `
-            -Name $rule.Name `
-            -Priority $rule.Priority `
-            -Action $rule.Action `
-            -IpAddress $rule.IpAddress `
-            -Description $rule.Description
-        A
-        Write-Host "Added IP restriction for $($rule.IpAddress)"
-    } else {
-        Write-Host "IP restriction for $($rule.IpAddress) already exists. Skipping..."
-    }
+    # Add the new access restriction rule
+    Add-AzWebAppAccessRestrictionRule -ResourceGroupName $resourceGroupName `
+        -WebAppName $appServiceName `
+        -Name $rule.Name `
+        -Priority $rule.Priority `
+        -Action $rule.Action `
+        -IpAddress $rule.IpAddress `
+        -Description $rule.Description
+            
+    Write-Host "Added IP restriction for $($rule.IpAddress)"
 }
 
 Write-Host "IP restrictions processed successfully!"
